64ec42989430f9db95bb3070950147118554ea60
IT24102297 <it24102297@my.sliit.lk>
2024-10-02 21:01:17 +0530

    Lab7Q2B

---
+++ Q2/IT24102297Lab7Q2B.java
@@ -0,0 +1,19 @@
+public class IT24102297Lab7Q2B{
+	public static void main(String args[]){
+
+	int i=0 , j=0;
+
+	for(i=1; i<=5; i++)
+	{
+	System.out.print(i + " - ");
+		
+		for(j=1; j<=i;j++)
+		{
+			System.out.print("* ");
+		}
+	System.out.println();
+	}
+	}
+}
+	
+	
